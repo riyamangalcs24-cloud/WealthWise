f7f43ea7917fc242da3be8993ace679b679fe6f3
dhruvsharda12 <dhruvsharda12@gmail.com>
2026-09-06 21:31:48 +0530

    correction

--- frontend/wealthwise/src/App.jsx
+++ frontend/wealthwise/src/App.jsx
@@ -1,32 +1,17 @@
-// added successfully
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import { useState } from 'react';
-import RefrshHandler from './RefrshHandler';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  // Authentication state
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Protected route component to restrict unauthenticated access
-  const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />;
-  };
-
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
-      </Routes>
-    </div>
-  );
+    <>
+      <h1 className="centre">
+        WealthWise
+      </h1>
+      
+
+    </>
+  )
 }
 
-export default App;
+export default App
